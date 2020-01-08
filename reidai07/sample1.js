ae1376e6abad272eca12c6d20b0ff49a6d81c758
saba383810 <saba383810@gmail.com>
2020-01-08 12:31:57 +0900

    commit

---
+++ reidai07/sample1.js
@@ -0,0 +1,26 @@
+//初期化
+var sum = 0;
+var avg = 0;
+var min = 100;
+var Max = 0;
+var dif;
+//乱数を10個表示
+for(var i = 0; i<10;i++){
+    var random = Math.floor( Math.random() * 89 ) + 10;
+    sum += random;
+    if(min>random){
+        min = random;
+    }
+    if(Max< random){
+        Max = random;
+    }
+    document.write(random," ");
+}
+avg = sum /10; // 平均値の計算
+dif = Max-min; // 最大値と最小値の差
+// 計算結果を表示
+document.write("<p>合計 : ",sum,"</p>");
+document.write("<p>平均 : ",avg,"</p>")
+document.write("<p>最大値 : ",Max,"</p>");
+document.write("<p>最小値 : ",min,"</p>");
+document.write("<p>最大値と最小値の差 : ",dif,"</p>");
