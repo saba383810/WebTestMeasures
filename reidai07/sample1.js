3749877665028c4d20ac12e06d05e47aa06c1bf8
saba383810 <saba383810@gmail.com>
2020-01-08 12:41:50 +0900

    commit

--- reidai07/sample1.js
+++ reidai07/sample1.js
@@ -8,19 +8,15 @@ var dif;
 for(var i = 0; i<10;i++){
     var random = Math.floor( Math.random() * 89 ) + 10;
     sum += random;
-    if(min>random){
-        min = random;
-    }
-    if(Max< random){
-        Max = random;
-    }
+    if(min>random) min = random;
+    if(Max< random) Max = random;
     document.write(random," ");
 }
 avg = sum /10; // 平均値の計算
-dif = Max-min; // 最大値と最小値の差
+diff = Max-min; // 最大値と最小値の差
 // 計算結果を表示
 document.write("<p>合計 : ",sum,"</p>");
 document.write("<p>平均 : ",avg,"</p>")
 document.write("<p>最大値 : ",Max,"</p>");
 document.write("<p>最小値 : ",min,"</p>");
-document.write("<p>最大値と最小値の差 : ",dif,"</p>");
+document.write("<p>最大値と最小値の差 : ",diff,"</p>");
