b9f9ad5dad8b86e86ac4ed3171c3340b0817f684
saba383810 <saba383810@gmail.com>
2020-01-09 20:36:19 +0900

    インデントを追加

--- reidai10/bingo-card.js
+++ reidai10/bingo-card.js
@@ -29,25 +29,25 @@ function setSquare(thisSquare) {
   document.getElementById(currSquare).onmousedown = toggleColor;
 }
 function getNewNum() {
-return Math.floor(Math.random() * 15);
+  return Math.floor(Math.random() * 15);
 }
 function anotherCard() {
-for (var i=1; i<usedNums.length; i++) {
-usedNums[i] = false;
+  for (var i=1; i<usedNums.length; i++) {
+    usedNums[i] = false;
   }
 newCard();
 return false;
 }
 
 function toggleColor(evt) {
-   if (evt) {
-      var thisSquare = evt.target;
-   } else {
-     var thisSquare = window.event.srcElement;
-   }
+  if (evt) {
+    var thisSquare = evt.target;
+  } else {
+    var thisSquare = window.event.srcElement;
+  }
 
   if (thisSquare.className == "") {
-     thisSquare.className = "pickedBG";
+    thisSquare.className = "pickedBG";
   } else {
     thisSquare.className = "";
   }
@@ -56,27 +56,27 @@ function toggleColor(evt) {
 }
 
 function checkWin() {
-var winningOption = -1;
-var setSquares = 0;
-var winners = new Array(31,992,15360,507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
-for (var i=0; i<24; i++) {
-var currSquare = "square" + i;
-if (document.getElementById (currSquare).className != "") {
-document.getElementById (currSquare).className = "pickedBG";
-setSquares = setSquares | Math.pow(2,i);
-}
-}
-for (var i=0; i<winners.length; i++) {
-if ((winners[i] & setSquares) == winners[i]) {
-winningOption = i;
-}
-}
-if (winningOption > -1) {
-for (var i=0; i<24; i++) {
-if (winners[winningOption] & Math.pow(2,i)) {
-currSquare = "square" + i;
-  document.getElementById (currSquare).className = "winningBG";
-}
-}
-}
+  var winningOption = -1;
+  var setSquares = 0;
+  var winners = new Array(31,992,15360,507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
+  for (var i=0; i<24; i++) {
+    var currSquare = "square" + i;
+    if (document.getElementById (currSquare).className != "") {
+      document.getElementById (currSquare).className = "pickedBG";
+      setSquares = setSquares | Math.pow(2,i);
+    }
+  }
+  for (var i=0; i<winners.length; i++) {
+    if ((winners[i] & setSquares) == winners[i]) {
+      winningOption = i;
+    }
+    }
+  if (winningOption > -1) {
+    for (var i=0; i<24; i++) {
+      if (winners[winningOption] & Math.pow(2,i)) {
+      currSquare = "square" + i;
+      document.getElementById (currSquare).className = "winningBG";
+      }
+    }
+  }
 }
