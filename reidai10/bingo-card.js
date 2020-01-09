3684065c4fd1b3a081eb3d1a1387e6a7a088a3fd
saba383810 <saba383810@gmail.com>
2020-01-09 20:17:47 +0900

    commit

--- reidai10/bingo-card.js
+++ reidai10/bingo-card.js
@@ -1,30 +1,30 @@
 window.onload = initAll;
 var usedNums = new Array(76);
 function initAll() {
-if (document.getElementById) {
-document.getElementById("reload").onclick = anotherCard;
-newCard();
-}
-else {
-alert("Sorry, your browser doesn't support this script");
-}
+  if (document.getElementById) {
+    document.getElementById("reload").onclick = anotherCard;
+    newCard();
+  }
+  else {
+    alert("Sorry, your browser doesn't support this script");
+  }
 }
 function newCard() {
-for (var i=0; i<24; i++) {
-setSquare(i);
-}
+  for (var i=0; i<24; i++) {
+    setSquare(i);
+  }
 }
 function setSquare(thisSquare) {
-var currSquare = "square" + thisSquare;
-var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
-var colBasis = colPlace[thisSquare] * 15;
-var newNum;
-do {
-newNum = colBasis + getNewNum() + 1;
-}
-while (usedNums[newNum]);
-usedNums[newNum] = true;
-document.getElementById(currSquare).innerHTML = newNum;
+  var currSquare = "square" + thisSquare;
+  var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+  var colBasis = colPlace[thisSquare] * 15;
+  var newNum;
+  do {
+    newNum = colBasis + getNewNum() + 1;
+  }
+  while (usedNums[newNum]);
+  usedNums[newNum] = true;
+  document.getElementById(currSquare).innerHTML = newNum;
   document.getElementById(currSquare).className = "";
   document.getElementById(currSquare).onmousedown = toggleColor;
 }
